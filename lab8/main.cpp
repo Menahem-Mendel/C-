dff3e5f9afd9258d13c5650da36840bc7944d863
Mendel Gelfand <mendelgel@gmail.com>
2019-05-31 08:57:47 +0300

    lab8

--- lab8/main.cpp
+++ lab8/main.cpp
@@ -12,52 +12,35 @@ int main()
 		d[SPACE * 2] = {0};
 	char file[50];
 
+	cout << "\ncopy this path: lab8/inputA.txt" << endl;
+	cin >> file;
+	if (fileInArray(file, a) == 0)
 	{
-		cout << "\ncopy this path: lab8/inputA.txt" << endl;
-		cin >> file;
-		if (fileInArray(file, a) == 0)
-		{
-			{
-				cout << "+++++++++++++++++++++++++			a 			+++++++++++++++++++++++++" << endl;
-				printArray(a, SPACE);
-			}
 
-			{
-				copyodd(a, c);
-				copyeven(a, d);
-			}
-		}
+		cout << "+++++++++++++++++++++++++			a 			+++++++++++++++++++++++++" << endl;
+		printArray(a, SPACE);
+
+		copyodd(a, c);
+		copyeven(a, d);
 	}
 
+	cout << "\ncopy this path: lab8/inputB.txt" << endl;
+	cin >> file;
+	if (fileInArray(file, b) == 0)
 	{
-		cout << "\ncopy this path: lab8/inputB.txt" << endl;
-		cin >> file;
-		if (fileInArray(file, b) == 0)
-		{
-			{
-				cout << "+++++++++++++++++++++++++			b 			+++++++++++++++++++++++++" << endl;
-				printArray(b, SPACE);
-			}
 
-			{
-				copyodd(b, c);
-				copyeven(b, d);
-			}
-		}
+		cout << "+++++++++++++++++++++++++			b 			+++++++++++++++++++++++++" << endl;
+		printArray(b, SPACE);
+
+		copyodd(b, c);
+		copyeven(b, d);
 	}
 
-	{
-		{
-			cout << "+++++++++++++++++++++++++			c 			+++++++++++++++++++++++++" << endl;
-			printArray(c, SPACE * 2);
-		}
+	cout << "+++++++++++++++++++++++++			c 			+++++++++++++++++++++++++" << endl;
+	printArray(c, SPACE * 2);
 
-		{
-			cout << "+++++++++++++++++++++++++			d 			+++++++++++++++++++++++++" << endl;
-			printArray(d, SPACE * 2);
-		}
-	}
+	cout << "+++++++++++++++++++++++++			d 			+++++++++++++++++++++++++" << endl;
+	printArray(d, SPACE * 2);
 
 	return 0;
 }
-
